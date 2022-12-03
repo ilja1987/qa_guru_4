143eb8b5d400694fa4e7bf918e339e67f091c841
ПК <domniniv@mail.ru>
2022-12-03 18:06:29 +0400

    text

--- qa_guru_4.py
+++ qa_guru_4.py
@@ -1,8 +1,12 @@
 def open_browser(browser_name):
     pass
 
+
+
 def go_to_companyname_homapage(page_url):
     pass
+
+
 def find_registration_button_on_login_page(page_url, button_text):
     pass
 
