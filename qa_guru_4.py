802a631c29d58d3400e8becfaaaede47bd86d945
ПК <domniniv@mail.ru>
2022-12-03 18:55:39 +0400

    text

--- qa_guru_4.py
+++ qa_guru_4.py
@@ -1,12 +1,20 @@
+def pr_func(name_func, *args):
+    name = name_func.__name__.title().replace('_', ' ') + ":"
+    print(name, *args)
+
+
 def open_browser(browser_name):
-    pass
+    pr_func(open_browser, browser_name)
 
 
-
-def go_to_companyname_homapage(page_url):
-    pass
+def go_to_companyname_homepage(page_url):
+    pr_func(go_to_companyname_homepage, page_url)
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    pass
+    pr_func(find_registration_button_on_login_page, page_url, button_text)
 
+
+open_browser(browser_name="Chrome")
+go_to_companyname_homepage(page_url="https://ya.ru/")
+find_registration_button_on_login_page(page_url="https://ya.ru/", button_text="Find")
